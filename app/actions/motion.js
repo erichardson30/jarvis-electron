9718f61be6236d57a8792a3d01f4509ca34c8b49
Chris Becom <cbecom@cardinalsolutions.com>
2016-05-03 15:31:00 -0400

    Added moment and synthesized speech

--- app/actions/motion.js
+++ app/actions/motion.js
@@ -1,6 +1,8 @@
 
 const exec = require('child_process').exec;
 
+var moment = require("moment");
+
 var gpio = require("gpio");
 var gpio4 = gpio.export(4, {
    direction: "in",
@@ -8,19 +10,28 @@ var gpio4 = gpio.export(4, {
    }
 });
 
+var timestamp = moment().add(1, 'm');
+
 // bind to the "change" event
 gpio4.on("change", function(val) {
+
     // value will report either 1 or 0 (number) when the value changes
     console.log(val)
     if (val == 1) {
-      console.log("turning on screen")
-      const child = exec('xset s reset && xset dpms force on',
-        (error, stdout, stderr) => {
-          console.log(`stdout: ${stdout}`);
-          console.log(`stderr: ${stderr}`);
-          if (error !== null) {
-            console.log(`exec error: ${error}`);
-          }
-      });
+      var now = moment()
+      if (now > timestamp) {
+        console.log("turning on screen")
+        responsiveVoice.speak("Hello, I am Jarvis. Welcome to Cardinal", "UK English Male");
+        const child = exec('xset s reset && xset dpms force on',
+          (error, stdout, stderr) => {
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+            if (error !== null) {
+              console.log(`exec error: ${error}`);
+            }
+        });
+      }
+    } else {
+      timestamp = moment().add(1, 'm');
     }
 });
