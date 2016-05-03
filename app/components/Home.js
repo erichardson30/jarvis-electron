5ab027f4b86aa021cfa8baf929ea303587578ef5
Eric <eric.richardson30@gmail.com>
2016-05-03 15:57:06 -0400

    encoding image file and sending it through sockets

--- app/components/Home.js
+++ app/components/Home.js
@@ -5,12 +5,17 @@ import io from 'socket.io-client';
 import jarvis from 'file!../jarvis-bkrd.png';
 import RaisedButton from 'material-ui/lib/raised-button';
 import ActionRecordVoiceOver from 'material-ui/lib/svg-icons/action/record-voice-over';
+import fs from 'fs';
 
-let socket = io(`http://10.104.100.41:8000`)
+let socket = io(`http://10.104.100.30:8000`)
 
 export default class Home extends Component {
 
   sendMessage = () => {
+    fs.readFile('./app/ironman.jpg', function(err, buf) {
+      socket.emit('image', { image: true, buffer: buf.toString('base64') });
+    })
+    
     socket.emit('notifyBot', "I'M HERE", function (err) {
       if (err) {
         return console.error("Socket error" + err);
@@ -33,6 +38,15 @@ export default class Home extends Component {
             onClick={this.sendMessage}
             icon={<ActionRecordVoiceOver />} 
           />
+          <RaisedButton
+            backgroundColor="#218EC1"
+            className={styles.button}
+            style={style2}
+            label="Let someone know I'm here" 
+            labelColor="#FFF"
+            onClick={this.sendMessage}
+            icon={<ActionRecordVoiceOver />} 
+          />
         </div>
       </div>
     );
@@ -42,6 +56,13 @@ const style = {
   position: 'fixed',
   bottom: '0px',
   left: '0px',
+  height: '60px',
+  width: '300px'
+}
+const style2 = {
+  position: 'fixed',
+  bottom: '0px',
+  right: '0px',
   height: '60px',
   width: '300px'
 }
