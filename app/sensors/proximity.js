70fe8d7ad3a124d22cad5d6c7bea714c248ed85e
Chris Becom <cbecom@cardinalsolutions.com>
2016-05-24 10:09:04 -0400

    Tweaking Proximity - added while loop for continuous monitoring

--- app/sensors/proximity.js
+++ app/sensors/proximity.js
@@ -1,4 +1,5 @@
 var gpio = require("gpio");
+var moment = require("moment");
 
 // pin 38 GPIO 20 - for trigger
 let trig = gpio.export(38, {
@@ -14,42 +15,54 @@ let echo = gpio.export(37, {
    }
 });
 
+
+
 var getDistance = function() {
 
   trig.set(function() {
 
-    var pulseStart = new Date();
-    var pulseEnd = new Date();
+    var timestamp = moment().add(30, 's');
+    var pulseStart = moment().millisecond();
+    var pulseEnd = moment().millisecond();
+    var timestamp = moment().add(1, 'm');
 
     trig.set(function() {
       setTimeout(function() {
         trig.set(0, function() {
-          echo.on("change", function(val) {
 
-            if (val = 1) {
+          let monitor = true;
+          let now = moment();
 
-              responsiveVoice.speak(" ");
-              pulseEnd = new Date();
-              let duration = pulseEnd.getTime() - pulseStart.getTime();
-              console.log("duration: " + duration);
+          // monitoring while loop
+          while(now.isBefore(timestamp) && monitor) {
+            echo.on("change", function(val) {
+              if (val = 1) {
 
-              if (duration < 300000) {
-                console.log("jarvis welcome")
-                responsiveVoice.speak("Hello I am Jarvis, welcome to Cardinal Solutions. Please check in", "UK English Male", {rate: 0.8});
+                responsiveVoice.speak(" ");
+                pulseEnd = new Date();
+                let duration = pulseEnd.getTime() - pulseStart.getTime();
+                console.log("duration: " + duration);
+
+                if (duration < 300000) {
+                  console.log("jarvis welcome")
+                  responsiveVoice.speak("Hello I am Jarvis, welcome to Cardinal Solutions. Please check in", "UK English Male", {rate: 0.8});
+                  monitor = false
+                }
+
+                let distance = duration * 17150;
+                let centimeters = Math.round(duration * 100) / 100;
+                console.log("centimeters: " + centimeters);
               }
+            });
+          }
 
-              let distance = duration * 17150;
-              let centimeters = Math.round(duration * 100) / 100;
-              console.log("centimeters: " + centimeters);
+          console.log("out of while loop")
+          trig.reset();
+          echo.reset();
 
-            } else {
-
-              console.log("nothing")
-              pulseStart = new Date();
-            }
-          });
         });
       }, 10);
+
     });
   });
 };
