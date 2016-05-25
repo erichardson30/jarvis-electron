8e7b77639bb7e0683b4f78e46bc2d7c97d6a818e
Chris Becom <cbecom@cardinalsolutions.com>
2016-05-25 11:08:54 -0400

    Tweaking Proximity loop 3

--- app/sensors/proximity.js
+++ app/sensors/proximity.js
@@ -4,7 +4,7 @@ var moment = require("moment");
 // pin 38 GPIO 20 - for trigger
 let trig = gpio.export(38, {
    direction: "out",
-
+   interval: 10,
    ready: function() {
    }
 });
@@ -17,70 +17,49 @@ let echo = gpio.export(37, {
    }
 });
 
-
-
 var getDistance = function() {
-
-  var monitor = true;
-  var timestamp = moment().add(30, 's');
-  var now = moment();
 
   // resetting trigger
   trig.set(0);
   console.log("initial trig value (0): " + trig.value) // should be 0
 
-  while(now.isBefore(timestamp) && monitor) {
+  var monitor = true;
+  var timestamp = moment().add(30, 's');
 
-    console.log("monitoring change");
-    setTimeout(function() {
+  // set up recursive loop
+  function monitorWithTimeout() {
 
-        var pulseStart = moment().millisecond();
-        var pulseEnd = moment().millisecond();
+    var pulseStart = moment().millisecond();
 
-        trig.set(function() {
+    // trigger on, then off then  monitor on change
+    trig.set(function() {
 
-          console.log("1 trig value (1): " + trig.value) // should be 1
-          setTimeout(function() {
+      console.log("1 trig value (1): " + trig.value) // should be 1
+      setTimeout(function() {
 
-            trig.set(0, function() {
+        trig.set(0, function() {
+           console.log("2 trig value (0): " + trig.value) // should be 0
 
-              console.log("2 trig value (0): " + trig.value) // should be 0
-              // on change
-
-              echo.on("change", function(val) {
-                console.log("on change")
-                if (val = 1) {
-
-                  responsiveVoice.speak(" ");
-                  pulseEnd = new Date();
-                  let duration = pulseEnd - pulseStart;
-                  console.log("duration: " + duration);
-
-                  if (duration < 300000) {
-                    console.log("jarvis welcome");
-                    responsiveVoice.speak("Hello I am Jarvis, welcome to Cardinal Solutions. Please check in", "UK English Male", {rate: 0.8});
-                    monitor = false
-                  }
-
-                  let distance = duration * 17150;
-                  let centimeters = Math.round(duration * 100) / 100;
-                  console.log("centimeters: " + centimeters);
-                }
-              });
-
-            });
-
-          }, 10);
+        // on change
 
         });
+      }, 15);
+    });
+    setTimeout(function () {
+
+      if (moment().isBefore(timestamp) && monitor) {
+        monitorWithTimeout();
+      };
 
     }, 1000);
-    now = moment();
+
+    // cleanup
+    console.log("cleanup");
+    echo.removeAllListeners('change');
+    trig.reset();
+    echo.reset();
   }
 
-  console.log("out of while loop");
-  trig.reset();
-  echo.reset();
 };
 
 module.exports = {
