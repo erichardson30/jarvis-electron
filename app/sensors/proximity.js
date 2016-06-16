8d39e6fb9f75e3f8d36c177aad7563bb6b56a912
Eric <eric.richardson30@gmail.com>
2016-06-16 16:01:41 -0400

    updated proximity file to simple example

--- app/sensors/proximity.js
+++ app/sensors/proximity.js
@@ -1,73 +1,83 @@
-var gpio = require("gpio");
-var moment = require("moment");
+import gpio from 'gpio';
+import moment from 'moment';
 
 // pin 38 GPIO 20 - for trigger
-let trig = gpio.export(38, {
-   direction: "out",
-   interval: 10,
-   ready: function() {
-   }
+const trig = gpio.export(38, {
+  direction: 'out'
 });
 
 // pin 37 GPIO 26 - for echo
-let echo = gpio.export(37, {
-   direction: "in",
-   interval: 50,
-   ready: function() {
-   }
+const echo = gpio.export(37, {
+  direction: 'in'
 });
 
-var getDistance = function() {
-
+function getDistance() {
   // resetting trigger
   trig.set(0);
+  setTimeout(500);
   console.log("initial trig value (0): " + trig.value); // should be 0
+  responsiveVoice.speak('');
+  const timestamp = moment().add(30, 's');
+  let start = 0;
+  let end = 0;
 
-  responsiveVoice.speak(" ");
-  var monitor = true;
-  var timestamp = moment().add(30, 's');
+  while (moment().isBefore(timestamp)) {
+    trig.set();
+    setTimeout(10);
+    trig.set(0);
 
-  // set up recursive loop
-  function monitorWithTimeout() {
+    while (echo.value === 0) {
+      start = moment();
+      console.log('Echo is 0');
+    }
 
-    console.log("monitoring");
-    var pulseStart = moment().millisecond();
-
-    // trigger on, then off then  monitor on change
-    trig.set(function() {
-
-      console.log("1 trig value (1): " + trig.value) // should be 1
-      setTimeout(function() {
-
-        trig.set(0, function() {
-           console.log("2 trig value (0): " + trig.value) // should be 0
-
-           console.log("echo value: " + echo.value)
-           echo.on("change", function(val) {
-
-             console.log("on change")
-
-           });
-        });
-
-      }, 15);
-
-    });
-
-    setTimeout(function () {
-
-      console.log("")
-      // echo.removeAllListeners('change');
-      if (moment().isBefore(timestamp) && monitor) {
-        monitorWithTimeout();
-      };
-
-    }, 2000);
+    while (echo.value === 1) {
+      end = moment();
+      console.log('Echo is 1');
+    }
   }
+  console.log('Start time = ' + start + ' End Time = ' + end);
+  // set up recursive loop
+  // function monitorWithTimeout() {
 
-  monitorWithTimeout();
-};
+  //   console.log("monitoring");
+  //   var pulseStart = moment().millisecond();
+
+  //   // trigger on, then off then  monitor on change
+  //   trig.set(function() {
+
+  //     console.log("1 trig value (1): " + trig.value) // should be 1
+  //     setTimeout(function() {
+
+  //       trig.set(0, function() {
+  //          console.log("2 trig value (0): " + trig.value) // should be 0
+
+  //          console.log("echo value: " + echo.value)
+  //          echo.on("change", function(val) {
+
+  //            console.log("on change")
+
+  //          });
+  //       });
+
+  //     }, 15);
+
+  //   });
+
+  //   setTimeout(function () {
+
+  //     console.log("")
+  //     // echo.removeAllListeners('change');
+  //     if (moment().isBefore(timestamp) && monitor) {
+  //       monitorWithTimeout();
+  //     };
+
+  //   }, 2000);
+  // }
+
+  // monitorWithTimeout();
+}
 
 module.exports = {
-  getDistance : getDistance
+  getDistance
 };
