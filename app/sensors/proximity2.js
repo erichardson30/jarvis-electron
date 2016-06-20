7b85b37ae5bf6a3747bf595e0cdfecc3a81c330c
Eric <eric.richardson30@gmail.com>
2016-06-20 11:17:51 -0400

    added more logs

--- app/sensors/proximity2.js
+++ app/sensors/proximity2.js
@@ -9,33 +9,38 @@ const trig = gpio.export(38, {
 const echo = gpio.export(37, {
   direction: 'in'
 });
+const speedSound = 34000;
+responsiveVoice.speak('');
+let start = 0;
+let end = 0;
+let timeDiff = 0;
+let distance = 0;
 
 function getDistance() {
-  const speedSound = 34000;
-  responsiveVoice.speak('');
-  let start = 0;
-  let end = 0;
-  let timeDiff = 0;
-  let distance = 0;
-
   // resetting trigger
+  console.log('resetting trigger');
   trig.set(0);
   setTimeout(function() {
     console.log("initial trig value (0): " + trig.value); // should be 0
+    console.log('turning on sensor');
     trig.set();
     setTimeout(function() {
+      console.log('turning off sensor');
       trig.set(0);
       start = new Date().getTime / 1000;
+      console.log('start time set to ' + start);
       if (echo.value === 0) {
         console.log('Echo is zero');
         start = new Date().getTime / 1000;
+        console.log('start time reset to ' + start);
       } else {
         console.log('Echo is ' + echo.value);
 
         end = new Date().getTime() / 1000;
+        console.log('end time set to ' + end);
         timeDiff = end - start;
+        console.log('time diff = ' + timeDiff);
         distance = (timeDiff * speedSound) / 2;
-
         console.log('Distance = ' + distance);
 
         if (distance < 60) {
