f10c0fd695fd98800e8cc42a6f5220e1b003d7ae
Eric <eric.richardson30@gmail.com>
2016-06-27 11:31:20 -0400

    reverted back to chris files for sensors

--- app/sensors/motion.js
+++ app/sensors/motion.js
@@ -1,25 +1,65 @@
 
-import { exec } from 'child_process';
-import gpio from 'gpio';
-// import proximity from './proximity2';
+// import { exec } from 'child_process';
+// import gpio from 'gpio';
+// // import proximity from './proximity2';
 
-const gpio4 = gpio.export(4, {
-  direction: 'in',
+// const gpio4 = gpio.export(4, {
+//   direction: 'in',
+// });
+
+// // bind to the "change" event
+// gpio4.on('change', (val) => {
+//   // value will report either 1 or 0 (number) when the value changes
+
+//   if (val === 1) {
+//     console.log('turning on screen');
+//     exec('xset s reset && xset dpms force on',
+//       (error, stdout, stderr) => {
+//         if (error !== null) {
+//           console.log(`exec error: ${error}`);
+//         }
+//       });
+//     console.log('checking proximity');
+//     // proximity.getDistance();
+//   }
+// });
+
+
+const exec = require('child_process').exec;
+
+var moment = require("moment");
+var gpio = require("gpio");
+var gpio4 = gpio.export(4, {
+   direction: "in",
+   ready: function() {
+   }
 });
 
-// bind to the "change" event
-gpio4.on('change', (val) => {
-  // value will report either 1 or 0 (number) when the value changes
+var proximity = require('./proximity');
+var timestamp = moment().add(1, 'm');
 
-  if (val === 1) {
-    console.log('turning on screen');
-    exec('xset s reset && xset dpms force on',
-      (error, stdout, stderr) => {
-        if (error !== null) {
-          console.log(`exec error: ${error}`);
-        }
-      });
-    console.log('checking proximity');
-    // proximity.getDistance();
-  }
+// bind to the "change" event
+gpio4.on("change", function(val) {
+
+    // value will report either 1 or 0 (number) when the value changes
+    if (val == 1) {
+
+      console.log("checking if should turn on");
+      let now = moment();
+      if (now.isAfter(timestamp)) {
+        console.log("turning on screen");
+        const child = exec('xset s reset && xset dpms force on',
+          (error, stdout, stderr) => {
+            if (error !== null) {
+              console.log(`exec error: ${error}`);
+            }
+        });
+
+        console.log("checking proximity");
+        proximity.getDistance();
+      }
+
+    } else {
+      timestamp = moment().add(1, 'm');
+    }
 });
--- app/sensors/proximity.js
+++ app/sensors/proximity.js
@@ -1,93 +1,73 @@
-import gpio from 'gpio';
-import moment from 'moment';
+var gpio = require("gpio");
+var moment = require("moment");
 
 // pin 38 GPIO 20 - for trigger
-const trig = gpio.export(38, {
-  direction: 'out'
+let trig = gpio.export(38, {
+   direction: "out",
+   interval: 10,
+   ready: function() {
+   }
 });
 
 // pin 37 GPIO 26 - for echo
-const echo = gpio.export(37, {
-  direction: 'in'
+let echo = gpio.export(37, {
+   direction: "in",
+   interval: 50,
+   ready: function() {
+   }
 });
 
-function getDistance() {
-  const speedSound = 34000;
+var getDistance = function() {
+
   // resetting trigger
   trig.set(0);
-  setTimeout(500);
   console.log("initial trig value (0): " + trig.value); // should be 0
-  responsiveVoice.speak('');
-  const timestamp = moment().add(30, 's');
-  let start = 0;
-  let end = 0;
 
-  trig.set();
-  setTimeout(10);
-  trig.set(0);
+  responsiveVoice.speak(" ");
+  var monitor = true;
+  var timestamp = moment().add(30, 's');
 
-  while (echo.value === 0) {
-    start = new Date().getTime() / 1000;
-    console.log('Echo is ' + echo.value);
-  }
-
-  while (echo.value === 1) {
-    end = new Date().getTime() / 1000;
-    console.log('Echo is ' + echo.value);
-    let timeDiff = end - start;
-    let distance = (timeDiff * speedSound) / 2;
-
-    console.log('Distance = ' + distance);
-
-    if (distance < 60) {
-      console.log('User is within 2 ft. Enable voice');
-      responsiveVoice.speak('Hello, I am Jarvis; welcome to Cardinal Solutions. Please check in.');
-      return true;
-    } else {
-      return false;
-    }
-  }
   // set up recursive loop
-  // function monitorWithTimeout() {
+  function monitorWithTimeout() {
 
-  //   console.log("monitoring");
-  //   var pulseStart = moment().millisecond();
+    console.log("monitoring");
+    var pulseStart = moment().millisecond();
 
-  //   // trigger on, then off then  monitor on change
-  //   trig.set(function() {
+    // trigger on, then off then  monitor on change
+    trig.set(function() {
 
-  //     console.log("1 trig value (1): " + trig.value) // should be 1
-  //     setTimeout(function() {
+      console.log("1 trig value (1): " + trig.value) // should be 1
+      setTimeout(function() {
 
-  //       trig.set(0, function() {
-  //          console.log("2 trig value (0): " + trig.value) // should be 0
+        trig.set(0, function() {
+           console.log("2 trig value (0): " + trig.value) // should be 0
 
-  //          console.log("echo value: " + echo.value)
-  //          echo.on("change", function(val) {
+           console.log("echo value: " + echo.value)
+           echo.on("change", function(val) {
 
-  //            console.log("on change")
+             console.log("on change")
 
-  //          });
-  //       });
+           });
+        });
 
-  //     }, 15);
+      }, 15);
 
-  //   });
+    });
 
-  //   setTimeout(function () {
+    setTimeout(function () {
 
-  //     console.log("")
-  //     // echo.removeAllListeners('change');
-  //     if (moment().isBefore(timestamp) && monitor) {
-  //       monitorWithTimeout();
-  //     };
+      console.log("")
+      // echo.removeAllListeners('change');
+      if (moment().isBefore(timestamp) && monitor) {
+        monitorWithTimeout();
+      };
 
-  //   }, 2000);
-  // }
+    }, 2000);
+  }
 
-  // monitorWithTimeout();
-}
+  monitorWithTimeout();
+};
 
 module.exports = {
-  getDistance
+  getDistance : getDistance
 };
